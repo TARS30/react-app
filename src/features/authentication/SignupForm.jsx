56bc6f429b6f8a37c27caf8e3d7737175507ddbb
TARS30 <chernovalexandr30@gmail.com>
2023-10-23 15:59:42 +0300

    test

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -5,7 +5,7 @@ import FormRow from "../../ui/FormRow";
 import Spinner from "../../ui/Spinner";
 
 import { useForm } from "react-hook-form";
-import { useSignup as useSignUp } from "./useSignup";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
@@ -14,7 +14,7 @@ function SignupForm() {
   
   const { errors } = formState;
 
-  const { signup, isLoading } = useSignUp();
+  const { signup, isLoading } = useSignup();
 
   function onSubmit({ fullName, email, password }) {
     if (isLoading) {
--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { signup as signupApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
-export function useSignUp() {
+export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
     onSuccess: () => {
